15b99df77032d0c52d20dff82ae044cb5170db1e
Lionel <lioneleoy@gmail.com>
2018-10-14 20:43:58 -0400

    Removed folder from repository

--- djangodash/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/lionelr/Documents/Development/petProject/DjangoDash/djangodash/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
